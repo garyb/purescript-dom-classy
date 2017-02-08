95be0761658a8a22e6d554988e1cdf661dd6320b
Gary Burgess <gary.burgess@gmail.com>
2017-02-08 14:17:23 +0000

    Add IsEvent varieties of Event functions

--- src/DOM/Classy/Event.purs
+++ src/DOM/Classy/Event.purs
@@ -1,18 +1,78 @@
 module DOM.Classy.Event where
 
 import Prelude
+
+import Control.Monad.Eff (Eff)
 import Control.Monad.Except (runExcept)
+
 import Data.Either (either)
 import Data.Foreign (toForeign)
 import Data.Maybe (Maybe(..))
+
+import DOM (DOM)
+import DOM.Event.Event as EE
+import DOM.Event.EventPhase (EventPhase)
 import DOM.Event.Types as E
 import DOM.HTML.Event.Types as HE
+import DOM.Node.Types (Node)
 import DOM.Websocket.Event.Types as WS
 
 -- | A class for subtypes of `Event`.
 class IsEvent e where
   toEvent :: e -> E.Event
   fromEvent :: E.Event -> Maybe e
+
+-- | The event type.
+type_ :: forall e. IsEvent e => e -> E.EventType
+type_ = EE.type_ <<< toEvent
+
+-- | The element that was the source of the event.
+target :: forall e. IsEvent e => e -> Node
+target = EE.target <<< toEvent
+
+-- | The element that the event listener was added to.
+currentTarget :: forall e. IsEvent e => e -> Node
+currentTarget = EE.currentTarget <<< toEvent
+
+-- | Indicates which phase of the event flow that is currently being processed
+-- | for the event.
+eventPhase :: forall e. (IsEvent e, Partial) => e -> EventPhase
+eventPhase = EE.eventPhase <<< toEvent
+
+-- | The integer value for the current event phase.
+eventPhaseIndex :: forall e. IsEvent e => e -> Int
+eventPhaseIndex = EE.eventPhaseIndex <<< toEvent
+
+-- | Prevents the event from bubbling up to futher event listeners. Other event
+-- | listeners on the current target will still fire.
+stopPropagation :: forall e eff. IsEvent e => e -> Eff (dom :: DOM | eff) Unit
+stopPropagation = EE.stopPropagation <<< toEvent
+
+-- | Prevents all other listeners for the event from being called. This includes
+-- | event listeners added to the current target after the current listener.
+stopImmediatePropagation :: forall e eff. IsEvent e => e -> Eff (dom :: DOM | eff) Unit
+stopImmediatePropagation = EE.stopImmediatePropagation <<< toEvent
+
+-- | Indicates whether the event will bubble up through the DOM or not.
+bubbles :: forall e. IsEvent e => e -> Boolean
+bubbles = EE.bubbles <<< toEvent
+
+-- | Indicates whether the event can be cancelled.
+cancelable :: forall e. IsEvent e => e -> Boolean
+cancelable = EE.cancelable <<< toEvent
+
+-- | Cancels the event if it can be cancelled.
+preventDefault :: forall e eff. IsEvent e => e -> Eff (dom :: DOM | eff) Unit
+preventDefault = EE.preventDefault <<< toEvent
+
+-- | Indicates whether `preventDefault` was called on the event.
+defaultPrevented :: forall e. IsEvent e => e -> Boolean
+defaultPrevented = EE.defaultPrevented <<< toEvent
+
+-- | The time in milliseconds between 01/01/1970 and when the event was
+-- | dispatched.
+timeStamp :: forall e. IsEvent e => e -> Number
+timeStamp = EE.timeStamp <<< toEvent
 
 instance isEventEvent :: IsEvent E.Event where
   toEvent = id
